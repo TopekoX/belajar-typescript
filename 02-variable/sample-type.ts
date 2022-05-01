9b3c6a9b43a6c94167dbf532f22e4ec0393e7ca7
Ucup Topekox <ucup.topekox@gmail.com>
2022-05-01 12:36:34 +0800

    demo variable

---
+++ 02-variable/sample-type.ts
@@ -0,0 +1,12 @@
+// mendefinisikan variable
+let active: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Naruto";
+let lastName: string = "Uzumaki";
+
+console.log(active);
+console.log("The Grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
+
+// use template string
+console.log(`Halo ${firstName} ${lastName}`);
